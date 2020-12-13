922b83542e22a7cc61c32c114504b1f07bf5c4a7
hgeorge <h.george16@yahoo.com>
2020-12-13 14:12:09 -0500

    fixed compile issue

--- include/interpolate.h
+++ include/interpolate.h
@@ -2,12 +2,12 @@
 #include <Eigen/Sparse>
 
 void interpolate(
-    const int nx,
-    const int ny,
-    const int nz,
-    const int dim,
-    const double h,
+    const int& nx,
+    const int& ny,
+    const int& nz,
+    const int& dim,
+    const double& h,
     const Eigen::RowVector3d& corner,
-    const Eigen::MatrixXd &q,
+    const Eigen::MatrixXd& q,
     Eigen::VectorXd& sum,
     Eigen::SparseMatrix<double>& W);
--- src/transfer_to_grid.cpp
+++ src/transfer_to_grid.cpp
@@ -9,20 +9,20 @@ void transfer_to_grid(Grid &grid, Particle &particles) {
 	int nx = grid.nx;
 	int ny = grid.ny;
 	int nz = grid.nz;
-	Eigen::Vector3d h = grid.h;
+	Eigen::RowVector3d h = grid.h.transpose();
 	Eigen::RowVector3d corner = grid.left_lower_corner.transpose();
 
 	Eigen::VectorXd sum; 
 	Eigen::VectorXd Vx, Vy, Vz;
-	interpolate(nx, ny, nz, 0, h(0), corner + h*Eigen::RowVector3d(0.5, 0, 0), particles.q, sum, Wx);
+	interpolate(nx, ny, nz, 0, h(0), corner + h.cwiseProduct(Eigen::RowVector3d(0.5, 0, 0)), particles.q, sum, Wx);
 	grid.Vx = Wx.transpose() * particles.v.col(0);
 	grid.Vx = (grid.Vx).cwiseQuotient(sum);
 
-	interpolate(nx, ny, nz, 1, h(1), corner + h*Eigen::RowVector3d(0, 0.5, 0), particles.q, sum, Wy);
+	interpolate(nx, ny, nz, 1, h(1), corner + h.cwiseProduct(Eigen::RowVector3d(0, 0.5, 0)), particles.q, sum, Wy);
 	grid.Vy = Wy.transpose() * particles.v.col(1);
 	grid.Vy = (grid.Vy).cwiseQuotient(sum);
 
-	interpolate(nx, ny, nz, 2, h(2), corner + h*Eigen::RowVector3d(0, 0, 0.5), particles.q, sum, Wz);
+	interpolate(nx, ny, nz, 2, h(2), corner + h.cwiseProduct(Eigen::RowVector3d(0, 0, 0.5)), particles.q, sum, Wz);
 	grid.Vz = Wz.transpose() * particles.v.col(2);
 	grid.Vz = (grid.Vz).cwiseQuotient(sum);
 
@@ -31,7 +31,7 @@ void transfer_to_grid(Grid &grid, Particle &particles) {
 
 	for (int n = 0; n < particles.q.rows(); n++) {
 		Eigen::RowVector3d x = particles.q.row(n) - grid.left_lower_corner.transpose();
-		Eigen::RowVector3d d = x.cwiseQuotient(h.transpose());
+		Eigen::RowVector3d d = x.cwiseQuotient(h);
 
 		int idx = int(d(0)) * (ny * nz) + int(d(1)) * nz + int(d(2));
 		grid.markers(idx) = int(FLUIDCELL);
